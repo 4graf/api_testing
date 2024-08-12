bd0cacbf78bf7ee7efdbcc650c851c46a4799244
4graf <senya.nabatov@mail.ru>
2024-08-12 15:04:08 +0300

    Добавление настроек и зависимостей

---
+++ settings.py
@@ -0,0 +1,18 @@
+"""
+Настройки проекта.
+"""
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class ApiSettings(BaseSettings):
+    """
+    Настройки для работы с тестируемым API.
+
+    :cvar base_url: Базовый url API.
+    """
+
+    base_url: str
